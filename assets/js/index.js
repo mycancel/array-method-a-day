9ee5c62b83afe4ec9d02c59ca7145ec82a2b1119
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-08-11 13:16:04 -0400

    Initial commit; created html page; linked js page; created two array variables

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const mainEl = document.querySelector('main');
+
+// TODO: Multiple Arrays depending on array method?
+const numArray = [1, 2, 8, 40, 500];
+const unsortedNumArr = [500, 5, 13, 8, 29];
+
