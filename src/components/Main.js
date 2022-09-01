a049691aab30020c2d3046835793f9c103edd5f9
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-08-31 23:45:45 -0400

    added images in methods.js; used useEffect to change method state; started to populate page with description and examples

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,29 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
+import methods from '../methods';
 
 const Main = ({ select }) => {
-  useEffect(() => console.log(select), [select]);
+  const [method, setMethod] = useState(select);
+
+  // When the select menu changes, the method displayed changes
+  useEffect(() => {
+    setMethod(methods.find((m) => m.method === select));
+  }, [select]);
+
+  // TODO: Populate Page with description and examples
   return (
-    <main>Main</main>
+    <>
+      {method ? 
+      (
+        <main>
+          <h1>{method.method}</h1>
+          <p>Hello</p>
+        </main>
+      ):(
+        <main>
+          <h1>Select a method to see more information</h1>
+        </main>
+      )}
+    </>
   )
 }
 
